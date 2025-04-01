fb832e25a2592ee3e74e962df0c55ba2af33fc3e
Gabriel Treviño <gabogagarren001@gmail.com>
2025-04-01 14:44:23 +0000

    create numbers

---
+++ age.c
@@ -0,0 +1,11 @@
+/*this program takes age input from the user 
+it stores the age variable and, print the value ussing printf()*/
+
+#include <stdio.h>
+
+int main(){
+    int age = 17; //create intenger variable
+
+    printf("Age: %d\n", age); //print the age variable
+    return(0);
+}
--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//Print "Hello world!"
 int main() {
 
     printf("Hello, World!\n");
