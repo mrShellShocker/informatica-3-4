79b2d3b8f9a67ceaa4f6eae1a50139f432868bed
Gabriel Treviño <gabogagarren001@gmail.com>
2025-04-03 16:30:23 +0000

    create variables

---
+++ variables-menu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+char snack1[] = "Chips";
+char snack2[] = "Soda";
+char snack3[] = "Candy";
+
+float snack1Value = 1.50;
+float snack2Value = 2.00;
+float snack3Value = 0.75;
+
+int snack1Stock = 20;
+int snack2Stock = 15;
+int snack3Stock = 30;
+
+int main(){
+
+    printf("Welcome to the Snack Shop\n");
+    printf("---------------------------\n");
+    printf("Item\t\tPrice\tQuantity\n");
+    //printf("%d snack,");
+}
