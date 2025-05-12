18f874eedda322e7f4c29f18026a60ca0de046c1
Gabriel Treviño <gabogagarren001@gmail.com>
2025-05-12 16:31:01 +0000

    bruhhh

--- exponents.c
+++ exponents.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<math.h>    
 
 int main (){
     int num;
     int exp;
     int count = 1;
+
 
     printf("Enter a number:");
     scanf("%d", &num);
@@ -11,12 +13,26 @@ int main (){
     printf("Enter the exponent: ");
     scanf("%d", &exp);
 
+    int option;
+
+    printf("Do you wanna use el while and go wild (1) or use power to get the pawa (2): ");
+    scanf("%d", &option);
+
     long long int ans = num;   
-     
-    while(count < exp){
+  
+    if (option = 1){
+        while(count < exp){
         ans = ans * num;
         ++count;
+        }
     }
+    else if (option = 2){
+        ans = pow(num, exp);
+    }
+    else{
+        printf("try again bruh.");
+    }
+    
     printf("%d ^ %d = %lli\n", num, exp, ans);
 
 
