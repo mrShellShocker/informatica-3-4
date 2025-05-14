8eb3ec33a0494dcfcc2b5f430450e67564e007d3
Gabriel Treviño <gabogagarren001@gmail.com>
2025-05-14 15:57:45 +0000

    chacabum chacabum

--- coin-flip.c
+++ coin-flip.c
@@ -9,7 +9,8 @@ int main() {
     scanf("%d", &guess);
 
     if (guess != 1 && guess != 2){
-        printf("Bruh its easy, 1 or 2\n");
+        printf("Bruh its easy, 1 or 2\nbueno pues try again\n");
+        return 0;
     }
 
     srand(time(NULL));
@@ -17,11 +18,17 @@ int main() {
     int coin = (rand() % 2) + 1;
 
     if (coin == 1 && coin == guess) { //heats//
-        printf("Heads\n");
+        printf("Congrats you got it, it was heads\n");
     }
-    else if ( coin == 1 && coin == guess ){ //tails//
-        printf("Tails\n");
-    }  
-
+    else if ( coin == 2 && coin == guess ){ //tails//
+        printf("Congrats you got it, it was tails\n");
+    }
+    else if (coin == 1 && coin != guess){ //wrong heats//
+        printf("Yyyy too bad it was heads\n");
+    }
+    else if (coin == 2 && coin != guess){//wrong tails//
+        printf("Yyyy too bad it was tails\n");
+    }
+ return 0;
 }
     
